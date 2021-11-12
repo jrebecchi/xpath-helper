0b7a7decb7adc392632234b5d332b3fa6b27c9b8
jrebecchi <jrebecchi@protonmail.com>
2021-11-12 01:20:33 +0100

    0.1.1 version statement

--- python/xpath-helper/tests/test_xpath_helper.py
+++ python/xpath-helper/tests/test_xpath_helper.py
@@ -2,7 +2,7 @@ from xpath_helper import __version__
 from xpath_helper import xh, filter
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.1'
 
 
 def test_get_parent(html_doc):
--- python/xpath-helper/xpath_helper/__init__.py
+++ python/xpath-helper/xpath_helper/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = ['xh', 'filter', 'XPathHelper', 'EmptyFilter']
 
 from xpath_helper.filter import EmptyFilter
